1b3cb0683172edae1c4234cfbbd0524b9e7c22eb
Annisa Cahya <annisacahyanisurya@gmail.com>
2022-07-19 15:50:36 +0700

    menambahkan paragraf baru

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> GIT Remote </title>
+</head>
+<body>
+    <h1> Hello world </h1>
+    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Dolores enim id facilis ex nisi perferendis aperiam laboriosam. 
+        Saepe tempora asperiores fuga cumque placeat beatae ullam vel voluptas architecto, quis facere.</p>
+</body>
+</html>
